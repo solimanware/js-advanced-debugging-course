5bb1d54a199e65442afaf128f42d354f399dab45
Osama Soliman <prof.o.soliman@gmail.com>
2022-05-15 16:05:17 +0200

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const addBtn = document.getElementById("addBtn");
+const result = document.getElementById("result");
+
+addBtn.addEventListener("click", () => {
+  const n1 = num1.value;
+  const n2 = num2.value;
+  result.innerHTML = n1 + n2;
+});
