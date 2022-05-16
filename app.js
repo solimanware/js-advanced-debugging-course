c7bdf4463d1f2c9484bd0d6d785628401c8f391e
Osama Soliman <prof.o.soliman@gmail.com>
2022-05-16 10:31:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,54 @@ addBtn.addEventListener("click", () => {
   const n2 = num2.value;
   result.innerHTML = n1 + n2;
 });
+
+
+// const studentDidAttend = false
+// console.assert(studentDidAttend, "Student did not attend class") //stacktrace
+// console.log("hi")
+// console.clear()
+function greeting() {
+  //console.log("hi");
+  console.count("hi is printed for this time");
+}
+// greeting();
+// greeting();
+// console.countReset("hi is printed for this time")
+// greeting();
+
+const car = {type:"Fiat", model:"500", color:"white"};
+//console.dir(num1)
+//console.table(car)
+
+// console.group()
+// console.log("hi")
+// console.log("hi")
+// console.log("hi")
+// console.groupEnd()
+
+// console.groupCollapsed()
+// console.log("hi")
+// console.log("hi")
+// console.log("hi")
+// console.groupEnd()
+
+// function calcTime(){
+//   console.time("setTimeout")
+//   setTimeout(()=>{
+//     console.timeEnd("setTimeout")
+//   },1000)
+// }
+// calcTime();
+
+// console.time()
+//  console.timeLog("answer time");
+// console.timeEnd()
+
+function foo() {
+  function bar() {
+    console.trace();
+  }
+  bar();
+}
+
+foo();
