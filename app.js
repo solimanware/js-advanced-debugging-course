20cbdb258043d74df7729e24395452f1445ff40a
Osama Soliman <prof.o.soliman@gmail.com>
2022-05-16 10:44:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ addBtn.addEventListener("click", () => {
   const n1 = num1.value;
   const n2 = num2.value;
   result.innerHTML = n1 + n2;
+  test();
 });
+
+
+function test(){
+  console.log("this is a test");
+}
