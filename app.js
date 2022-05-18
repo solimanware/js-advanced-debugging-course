fe6f8f0a5a6319e2a2a0c328f81d8c98ad0379b4
Osama Soliman <prof.o.soliman@gmail.com>
2022-05-18 14:54:37 +0200

    init

--- app.js
+++ app.js
@@ -4,7 +4,45 @@ const addBtn = document.getElementById("addBtn");
 const result = document.getElementById("result");
 
 addBtn.addEventListener("click", () => {
-  const n1 = num1.value;
-  const n2 = num2.value;
+  const n1 = Number(num1.value);
+  const n2 = Number(num2.value);
   result.innerHTML = n1 + n2;
 });
+
+// create car object
+const car = {
+  make: "Ford",
+  model: "Mustang",
+  year: "1969",
+  color: "red",
+};
+
+function a() {
+  console.log("this is function a ");
+  b();
+}
+
+function b() {
+  console.log("this is function b");
+  done();
+}
+
+function done() {
+  console.log("done");
+}
+
+a();
+
+const repos = document.getElementById("repos");
+
+//github fetch user microsmsm
+fetch("https://api.github.com/users/microsmsm/repos")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((repo) => {
+      const name = document.createTextNode(repo.name);
+      const tag = document.createElement("p");
+      tag.appendChild(name);
+      repos.appendChild(tag);
+    });
+  });
